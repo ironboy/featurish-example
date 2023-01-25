1ed0b05e07526326881dbc4d67c60402dacd0afa
Ironboy <ironboy@users.noreply.github.com>
2023-01-25 13:44:00 +0100

    Done! The feature works perfectly!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // useful hooks, comment these in, when needed:
 // import { useStates } from './utilities/states.js';
 // import { useEffect } from 'react';
+import MainMenu from "./MainMenu";
 
 export default function App() {
 
   return <>
+    <MainMenu />
     <h1>Hello world!</h1>
   </>;
 }
