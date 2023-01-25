85a7c3475e94aeac2f0f9b6c7bf2d2a2fcd84320
Ironboy <ironboy@users.noreply.github.com>
2023-01-25 13:49:13 +0100

    amazing footer done

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@
 // import { useStates } from './utilities/states.js';
 // import { useEffect } from 'react';
 
+import Footer from './Footer';
+
 export default function App() {
 
   return <>
     <h1>Hello world!</h1>
+    <Footer />
   </>;
 }
